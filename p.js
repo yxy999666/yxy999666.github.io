460cbd0c8f997da77e4ceec8a9cdb0699427345d
yxy999666 <128456348+yxy999666@users.noreply.github.com>
2024-08-02 19:44:37 +0800

    Update p.js

--- p.js
+++ p.js
@@ -1,37 +1,20 @@
-// 定义API URL，注意URL末尾的斜杠
-const apiUrl = "https://ossapi.ailiyuncs.com/";
-
-// 定义获取URL最后一个参数的函数
-function getUrlLastParam() {
-    const url = location.search;
-    const data = url.substring(url.lastIndexOf("=") + 1);
-    return data;
+function loadhtml(url){ 
+ var xhr = new XMLHttpRequest;
+ var html = null;
+ function render() {
+  var a = document.open("text/html", "replace");
+  a.write(html);
+  a.close();
+ }
+ xhr.onload = function () {
+  html = xhr.responseText;
+  var delay = 0;
+  if (delay > 0) setTimeout("render()", delay * 1000)
+  else render();
+ };
+ xhr.open("GET", url, !0);
+ xhr.send(); 
 }
 
-// 定义从查询字符串中获取指定参数的函数
-function getQueryString(name) {
-    const reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)");
-    const r = window.location.search.substr(1).match(reg);
-    if (r != null) {
-        return decodeURIComponent(r[2]);
-    }
-    return '';
-}
 
-// 获取localStorage中的"linecode"，或者查询字符串中的"lines"，或者URL最后一个参数
-const lines = localStorage.getItem("linecode") || getQueryString("lines") || getUrlLastParam();
-const signUrl = location.href.split("#")[0];
-
-// 使用fetch请求百度首页的HTML内容
-fetch("http://baidu.com")
-    .then(response => response.text()) // 获取响应的文本内容
-    .then(html => {
-        // 替换当前页面的HTML内容
-        document.open(); // 打开文档流
-        document.write(html); // 写入HTML内容
-        document.close(); // 关闭文档流
-    })
-    .catch(error => {
-        console.error('请求失败:', error);
-        // 处理请求错误，例如显示错误信息或跳转到备用页面
-    });
+ loadhtml('https://baidu.com')
